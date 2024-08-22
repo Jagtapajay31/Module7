67761264fb3b137816bb6f393d3007446a4a796e
ajay jagtap <jagtapajay64@gmail.com>
2024-08-22 11:27:05 +0530

    new

--- Module7/src/com/seleniumwebdriver/DropDropDown.java
+++ Module7/src/com/seleniumwebdriver/DropDropDown.java
@@ -1,24 +1,33 @@
 package com.seleniumwebdriver;
-/*
- *  W.A.J.Script for Locating links by linkText() and partialLinkText()
- */
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class LinkTextPartialLinkText {
+/*
+ *  W.A.J.Script for Selecting multiple items in a drop dropdown
+ */
+public class DropDropDown {
 	public static void main(String[] args) throws InterruptedException {
 //		System.setProperty("webdriver.edge.driver","E:\\Tops 2024\\Selenium\\msedgedriver.exe");
 //		WebDriver driver=new EdgeDriver();
 		System.setProperty("webdriver.chrome.driver","E:\\Tops 2024\\Selenium\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-//		driver.get("file:///C:\\Users\\os\\git\\repository3\\Module7\\src\\com\\seleniumwebdriver\\index1.html");
-		driver.get("https://phptravels.com/");
 		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/test/newtours/register.php");
+		Thread.sleep(2000);	
+
+		WebElement e1=driver.findElement(By.name("country"));
+		
+		
+		Select s1=new Select(e1);
+		s1.selectByIndex(10);
+		
 		Thread.sleep(2000);
 		
-//		driver.findElement(By.partialLinkText("Click")).click();
-		driver.findElement(By.partialLinkText("Share")).click();
+		s1.selectByValue("INDIA");
 		Thread.sleep(2000);
 		
 		driver.close();

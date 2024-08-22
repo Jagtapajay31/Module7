5648590274ca1d60fe7dd4105b0f3adcf6d6bb2c
ajay jagtap <jagtapajay64@gmail.com>
2024-08-22 10:08:00 +0530

    new

--- Module7/src/com/seleniumwebdriver/LinkTextPartialLinkText.java
+++ Module7/src/com/seleniumwebdriver/LinkTextPartialLinkText.java
@@ -1,5 +1,24 @@
 package com.seleniumwebdriver;
 
-public class LinkTextPartialLinkText {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class LinkTextPartialLinkText {
+	public static void main(String[] args) throws InterruptedException {
+//		System.setProperty("webdriver.edge.driver","E:\\Tops 2024\\Selenium\\msedgedriver.exe");
+//		WebDriver driver=new EdgeDriver();
+		System.setProperty("webdriver.chrome.driver","E:\\Tops 2024\\Selenium\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///C:\\Users\\os\\git\\repository3\\Module7\\src\\com\\seleniumwebdriver\\index1.html");
+//		driver.get("https://phptravels.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.partialLinkText("Click")).click();
+//		driver.findElement(By.partialLinkText("Share")).click();
+		Thread.sleep(2000);
+		
+		driver.close();
+	}
 }
